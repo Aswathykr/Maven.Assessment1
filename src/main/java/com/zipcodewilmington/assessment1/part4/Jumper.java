ef4ba4a6a3b2c6865b4a7771bd14596e23e9f665
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-02-18 12:56:32 -0500

    68 of 70 passed

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -6,6 +6,16 @@ public class Jumper {
      * Complete the function below.
      */
     public int jumps(int k, int j) {
-        return (int) j / k;
+        int count =0;
+        int step = k < j ? k : j; // step is the shorter one
+        int end =  k > j ? k : j; // end is the longer integer
+        int outerLoop =0;
+        for (; outerLoop < end; outerLoop+=step) {
+            count++;
+       }
+       if(outerLoop > end + 1)// consider last short jump
+           count++;
+        return count;
+
     }
 }
