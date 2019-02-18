cf9700e1cf620e095c168eb098d4d657366ae6a6
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-02-18 12:15:00 -0500

    part 5 - All testcase passed

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -3,6 +3,19 @@ package com.zipcodewilmington.assessment1.part5;
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-        return null;
+        int count =0;
+        for (int i = 2; i <= input.length(); i++) {
+            for(int j = 0; j <= input.length()- i;j++){
+                String subStr = input.substring(j, j+i);
+                if(isPalindrome(subStr))
+                    count++;
+            }
+        }
+        return count + input.length();
+    }
+
+    public boolean isPalindrome(String input){
+        StringBuilder builder = new StringBuilder(input);
+        return builder.reverse().toString().equals(input);
     }
 }
